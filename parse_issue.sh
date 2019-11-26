3f66e2c5d9409905bf69ca5787a37f7d0a9ae515
Daniyal <daniyaljavedk@gmail.com>
2019-11-26 17:53:46 -0500

    Update parse_issue.sh

--- parse_issue.sh
+++ parse_issue.sh
@@ -2,9 +2,9 @@
 curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$INPUT_REPO_NAME/issues --header "Content-Type:application/vnd.github.symmetra-preview+json" | jq '.[0] |  .body' > issues
 ls
 sed 's/\"//g' issues > latest_issue
-cat latest
+cat latest_issue
 git clone https://github.com/$INPUT_REPO_NAME
-mv latest issue $INPUT_REPO_NAME
+mv latest_issue $INPUT_REPO_NAME
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 cd $INPUT_REPO_NAME
