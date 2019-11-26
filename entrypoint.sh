a0d410c8090b86505eb1dede2d5c3c4e519cb78c
Daniyal <daniyaljavedk@gmail.com>
2019-11-26 16:34:11 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
-
-#curl -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -X GET -d @issue.json https://api.github.com/repos/$1 --header "Content-Type:application/vnd.github.symmetra-preview+json" | jq '.[0] |  .body' > issues
-
 env
+curl -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -X GET https://api.github.com/repos/$INPUT_REPO_NAME/issues --header "Content-Type:application/vnd.github.symmetra-preview+json" | jq '.[0] |  .body' > issues
