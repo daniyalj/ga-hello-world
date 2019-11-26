4fa6b19629dea8939dc3ab559eafe097cfc9b52e
Daniyal <daniyaljavedk@gmail.com>
2019-11-26 17:45:53 -0500

    Update parse_issue.sh

--- parse_issue.sh
+++ parse_issue.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
-curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$INPUT_REPO_NAME/issues --header "Content-Type:application/vnd.github.symmetra-preview+json" | jq '.[0] |  .body' > issues
+curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$INPUT_REPO_NAME/issues --header "Content-Type:application/vnd.github.symmetra-preview+json" | jq '.[0] |  .body' > /issues
 ls
-sed 's/\"//g' issues > latest_issue
+sed 's/\"//g' /issues > /latest_issue
 ls
 pwd
-cat latest_issue
+cat /latest_issue
