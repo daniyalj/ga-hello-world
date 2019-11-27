ce6f60d93c5bb27b557774a0cb0e826037203160
Daniyal <daniyaljavedk@gmail.com>
2019-11-27 11:01:09 -0500

    Update parse_issue.sh

--- parse_issue.sh
+++ parse_issue.sh
@@ -3,7 +3,9 @@ curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos
 ls
 sed 's/\"//g' issues > latest_issue
 cat latest_issue
+ls
 git clone https://github.com/$INPUT_ORG_NAME/$INPUT_REPO_NAME
+ls
 cat latest_issue
 mv latest_issue $INPUT_REPO_NAME
 git config --global user.email "action@github.com"
@@ -11,3 +13,4 @@ git config --global user.name "GitHub Action"
 cd $INPUT_REPO_NAME
 git add .
 git commit -m "Add latest_issue"
+git push
